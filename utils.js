1eae5cf9b08d6bf04f1e83939ed1f84be1108001
Mayaaalborg <maaa0002@stud.kea.dk>
2026-05-18 14:23:54 +0200

    kommentarer til utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,9 @@
 
+// authFetch er en wrapper omkring den indbyggede fetch funktion.
+// Den henter automatisk JWT-tokenen fra localStorage og tilføjer den
+// som Authorization header på alle requests, så man ikke behøver
+// at skrive headers: { 'Authorization': token } manuelt hver gang.
+
 export function authFetch(url, options = {}) {
     const token = localStorage.getItem('jwt')
     return fetch(url, {
